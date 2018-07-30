4bcb455ae4afa24d536595c89a3facc77dc68df3
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-30 04:06:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@
 def key_for_min_value(name_hash)
   min = nil
   lowkey = nil
-  name_has.each do |key, value|
+  name_hash.each do |key, value|
     if (min == nil || value < min)
       min = value
       lowkey = key
